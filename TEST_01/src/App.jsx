4ca18d4f94ef9869002fb1b10dded07309c20261
Velmurugan-02 <uvelmurugan218@gmail.com>
2026-01-13 13:06:33 +0530

    FIRST COMMIT WITH PROPS LEARNING ON DAY 2

---
+++ TEST_01/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Test_props from './Test_props';
+
+function App() {
+  let name = "Velmurugan";
+  return (
+    <>
+      <Test_props name = "Velmurugan" num = "1"/>
+    </>
+  )
+}
+
+export default App
---
+++ TEST_01/src/Test_props.jsx
@@ -0,0 +1,9 @@
+function Test_props(props){
+    return(
+        <>
+            <p>Hello {props.name} Intership Class no : {props.num}</p>
+        </>
+    )
+}
+
+export default Test_props;
