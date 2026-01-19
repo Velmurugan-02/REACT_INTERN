b3f76e136a1592a271ce14deb607aa6644f10401
Velmurugan-02 <uvelmurugan218@gmail.com>
2026-01-19 15:59:46 +0530

    TestProps.jsx tested component deleted

--- TEST_01/Components/TestProps.jsx
+++
@@ -1,9 +0,0 @@
-function Test_props(props){
-    return(
-        <>
-            <p>Hello {props.name} Intership Class no : {props.num}</p>
-        </>
-    )
-}
-
-export default Test_props;
--- TEST_01/src/App.jsx
+++ TEST_01/src/App.jsx
@@ -1,13 +1,7 @@
-import Test_props from '../Components/TestProps';
-
 function App() {
   let name = "Velmurugan";
   return (
     <>
-      <Test_props name = "Velmurugan" num = "1"/>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
     </>
   )
 }
