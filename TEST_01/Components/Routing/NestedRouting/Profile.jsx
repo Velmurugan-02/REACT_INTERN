a30ac05725531c1616c0ad6cb7fec8d480f03eca
Velmurugan-02 <uvelmurugan218@gmail.com>
2026-01-27 17:46:18 +0530

    Routing is implemented using the BrowserRouter , routes , route , Link and BrowserRouter notes is added for the reference

--- TEST_01/Components/Routing/NestedRouting/Profile.jsx
+++ TEST_01/Components/Routing/NestedRouting/Profile.jsx
@@ -1,18 +1,9 @@
-import { NavLink , Outlet } from "react-router-dom";
-import Settings from "./Settings";
-import Information from "./Information";
-
 let Profile = () => {
     return(
         <>
             <div>
                 <p>Profile</p>
             </div>
-            <nav>
-                <NavLink to="settings">Settings</NavLink>
-                <NavLink to="information">Information</NavLink>
-            </nav>
-            <Outlet />
         </>
     );
 }
--- TEST_01/src/App.jsx
+++ TEST_01/src/App.jsx
@@ -4,8 +4,6 @@ import Home from "../Components/Routing/Pages/Home";
 import About from "../Components/Routing/Pages/About";
 import Contact from "../Components/Routing/Pages/Contact";
 import Profile from "../Components/Routing/NestedRouting/Profile";
-import Settings from "../Components/Routing/NestedRouting/Settings";
-import Information from "../Components/Routing/NestedRouting/Information";
 function App() {
   return (
     <>
@@ -14,10 +12,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/profile" element={<Profile />}>
-            <Route path="settings" element={<Settings />} />
-            <Route path="Information" element={<Information />} />
-          </Route>
+          <Route path="/profile" element={<Profile />}></Route>
       </Routes>
     </>
   )
